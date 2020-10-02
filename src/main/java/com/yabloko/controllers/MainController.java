124181651870034647c94aac92e7b82378820518
Artem Yablokov <apple@rambler.ru>
2020-10-02 18:39:26 +0300

    v_4 mustache

--- src/main/java/com/yabloko/controllers/MainController.java
+++ src/main/java/com/yabloko/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.yabloko.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,13 @@ public class MainController {
     @GetMapping("/main")
     public String getMainPage(Model model) {
         System.out.println("req");
-        return "main";
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String recieve(@RequestParam("username") String userName,
+                          @RequestParam("password") String password){
+        System.out.println(userName + " " + password);
+        return "login";
     }
 }
